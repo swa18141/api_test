77cd453c05e388637bff7a29de7809c2d1954184
swa18141 <127288802+swa18141@users.noreply.github.com>
2023-09-27 23:33:49 +0530

    Create tests.ps1

---
+++ tests.ps1
@@ -0,0 +1,40 @@
+# Load the YAML module if not already loaded
+if (-not (Get-Module -Name 'powershell-yaml' -ListAvailable)) {
+    Install-Module -Name 'powershell-yaml' -Force -Scope CurrentUser
+}
+
+# Import the YAML module
+Import-Module 'powershell-yaml'
+
+# Read the YAML file
+$apiRequest = Convert-FromYaml -Path '.\api_request.yaml'
+
+# Define API request parameters
+$apiUrl = $apiRequest.url
+$apiMethod = $apiRequest.method
+$apiHeaders = $apiRequest.headers
+$queryParameters = $apiRequest.query_parameters
+
+# Build the query string from query parameters
+$queryString = [System.Web.HttpUtility]::ParseQueryString("")
+foreach ($param in $queryParameters.Keys) {
+    $queryString.Add($param, $queryParameters[$param])
+}
+$queryString = $queryString.ToString()
+
+# Combine URL and query string
+if ($queryString -ne '') {
+    $apiUrl = "$apiUrl?$queryString"
+}
+
+# Make the API request
+try {
+    $response = Invoke-RestMethod -Uri $apiUrl -Method $apiMethod -Headers $apiHeaders
+
+    # Print the response
+    Write-Host "Response:"
+    Write-Host $response
+}
+catch {
+    Write-Host "API request failed: $_"
+}
